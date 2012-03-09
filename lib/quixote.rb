fa590a9dedba591e32a132df0c29134a60c61126
Matt Sanders <matt@modal.org>
2012-03-09 17:17:12 -0600

    Add initial implementation.

--- lib/quixote.rb
+++ lib/quixote.rb
@@ -1,5 +1,42 @@
 require "quixote/version"
 
-module Quixote
-  # Your code goes here...
+class Quixote
+  attr_accessor :min, :max, :varies_by, :last
+
+  def initialize(options={})
+    defaults = {
+      :max => 100,
+      :min => 0,
+      :varies_by => 10
+    }
+
+    defaults.merge(options).each do |key, value|
+      send("#{key}=", value)
+    end
+
+    @last = rand(max+1)
+  end
+
+  def next
+    if last == max
+      decrement
+    elsif last == min || (rand < 0.5)
+      increment
+    else
+      decrement
+    end
+    last
+  end
+
+  private
+
+  def increment
+    @last += rand(varies_by+1)
+    @last = max if last > max
+  end
+
+  def decrement
+    @last -= rand(varies_by+1)
+    @last = min if last < min
+  end
 end
